0a63fca1d72bc93d78c846de15fad931bbddd990
aananev <6167006@bk.ru>
2024-02-21 22:06:31 +0300

    menu+bg

--- components/modules/Header/Menu.tsx
+++ components/modules/Header/Menu.tsx
@@ -1,7 +1,10 @@
-import React, { useState } from "react"
+import { $menuIsOpen, closeMenu } from "@/context/modals"
 import { useLang } from "@/hooks/useLang"
+import { removeOverflowHiddenFromBody } from "@/lib/utils/common"
 import { useUnit } from "effector-react"
-import { $menuIsOpen } from "@/context/modals"
+import { useState } from "react"
+import { setLang } from "@/context/lang"
+import { AllowedLangs } from "@/constants/lang"
 
 const Menu = () => {
   const [showCatalogList, setShowCatalogList] = useState(false)
@@ -9,11 +12,53 @@ const Menu = () => {
   const [showContactsList, setContactsList] = useState(false)
   const menuIsOpen = useUnit($menuIsOpen)
   const { lang, translations} = useLang()
+
+  const handleCloseMenu = () => {
+    removeOverflowHiddenFromBody()
+    closeMenu()
+  }
+
+  const handleSwitchLang = (lang: string) => {
+    setLang(lang as AllowedLangs)
+    localStorage.setItem('lang', JSON.stringify(lang))
+  }
+
+  const handleSwitchLangToRu = () => handleSwitchLang('ru')
+  const handleSwitchLangToEn = () => handleSwitchLang('en')
+
     return (
-        <nav className={`nav-menu %{menuIsOpen ? 'open' : 'close'}`}>
+        <nav className={`nav-menu ${menuIsOpen ? 'open' : 'close'}`}>
+          <img
+            className={`nav-menu__bg ${menuIsOpen ? 'open' : ''}`}
+            src='/img/menu-bg2%201.png'
+            alt='menu background'
+          />
+          <button
+            className={`btn-reset nav-menu__close ${menuIsOpen ? 'open' : ''}`}
+            onClick={handleCloseMenu}
+          />
+          <div  className={`nav-menu__lang ${menuIsOpen ? 'open' : ''}`}>
+            <button
+              className={`btn-reset nav-menu__lang__btn ${lang === 'ru' ? 'lang-active' : ''}`}
+              onClick={handleSwitchLangToRu}
+            >
+              RU
+            </button>
+            <button
+              className={`btn-reset nav-menu__lang__btn ${lang === 'en' ? 'lang-active' : ''}`}
+              onClick={handleSwitchLangToEn}
+            >
+              EN
+            </button>
+            <ul className={`list-reset nav-menu__list ${menuIsOpen ? 'open' : ''}`}>
+              <li className='nav-menu__list__item'></li>
+              <li className='nav-menu__list__item'></li>
+              <li className='nav-menu__list__item'></li>
+            </ul>
+          </div>
           <h1>Menu</h1>
         </nav>
-    );
-};
+    )
+}
 
-export default Menu;
+export default Menu
