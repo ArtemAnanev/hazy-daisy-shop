d71cece5f2ea98da459fb9f887640fbd2787b910
aananev <6167006@bk.ru>
2024-03-30 00:46:19 +0300

    manifestqwwer

--- app/manifest.ts
+++ app/manifest.ts
@@ -6,12 +6,21 @@ export default function manifest(): MetadataRoute.Manifest {
     short_name:  'Hazy Daisy App',
     description: 'Hazy Daisy магазин футболок для ваших тодлеров',
     start_url: '/',
+    background_color: '#fff',
+    theme_color: '#fff',
+    display: 'standalone',
     icons: [
       {
         src: '/img/icon.svg',
-        sizes: '196x196',
+        sizes: '196x196 512x512 144x144 192x192 128x128 120x120 180x180',
         type: 'image/svg',
         purpose: 'maskable',
+      },
+      {
+        src: '/img/logo.png',
+        sizes: '144x144 512x512 192x192 196x196 128x128 120x120 180x180',
+        type: 'image/png',
+        purpose: 'any',
       }
     ]
   }
