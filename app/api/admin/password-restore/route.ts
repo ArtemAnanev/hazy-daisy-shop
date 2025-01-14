ab3b96c3367b7063e73e08a1d412f342b8b36be5
aananev <6167006@bk.ru>
2025-01-14 12:28:10 +0300

    изменение пароля пользователя

---
+++ app/api/admin/password-restore/route.ts
@@ -0,0 +1,25 @@
+import { getDbAndReqBody } from "../../../../lib/utils/api-routes";
+import clientPromise from "../../../../lib/mongodb";
+import { sendMail } from "../../../../service/mailService";
+import { NextResponse } from "next/server";
+import { corsHeaders } from "../../../../constants/corsHeaders";
+
+export async function POST(req: Request) {
+  try {
+    const { reqBody } = await getDbAndReqBody(clientPromise, req);
+
+    await sendMail(
+      "Hazy daisy",
+      reqBody.email,
+      `Ссылка для сброса пароля: ${process.env.NEXT_PUBLIC_CLIENT_BASE_URL}/password-restore`,
+    );
+
+    return NextResponse.json({ status: 200 }, corsHeaders);
+  } catch (error) {
+    throw new Error((error as Error).message);
+  }
+}
+
+export async function OPTIONS() {
+  return new NextResponse(null, { ...corsHeaders, status: 200 });
+}
