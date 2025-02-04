f03c41181b077238c63bdb12d392e28c957309d3
Artem Ananev <56456957+ArtemAnanev@users.noreply.github.com>
2025-02-04 18:24:25 +0300

    Update index.tsx
    Update index.tsx

--- components/modules/Analytics/index.tsx
+++ components/modules/Analytics/index.tsx
@@ -1,7 +1,14 @@
 import GoogleAnalytics from "@/components/modules/Analytics/GoogleAnalytics/GoogleAnalytics";
+import YandexMetrika from "@/components/modules/Analytics/YandexMetrika/YandexMetrika";
 
 export const AnalyticScripts = () => {
   const gaId = process.env.NEXT_PUBLIC_GOOGLE_ANALITICS_ID;
+  const ymId = process.env.NEXT_PUBLIC_YANDEX_METRICA_ID;
 
-  return <>{Boolean(gaId) && <GoogleAnalytics id={gaId || ''} />}</>
+  return (
+    <>
+      {Boolean(gaId) && <GoogleAnalytics id={gaId || ''} />}
+      {Boolean(ymId) && <YandexMetrika id={ymId || ''} />}
+    </>
+  )
 }
